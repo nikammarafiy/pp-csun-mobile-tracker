b43bbd313bfa9621e33ab5809233ecb88d09253a
kl366734 <kl366734@b3c8902e-b84e-8e89-50af-a8b036748bb7>
2011-02-06 04:28:13 +0000

    ParamClass.java Document update

--- src/net/ParamClass.java
+++ src/net/ParamClass.java
@@ -2,10 +2,15 @@ package net;
 
 import java.io.Serializable;
 
+	/*
+	 * Purpose of this class is to keep the configuration initialized by parents.
+	 */
+
 public class ParamClass implements Serializable {
 
 	/**
-	 * 
+	 *  @param loiteringTime Maximum number of seconds that a child must stay in a same spot.
+	 *  @param deviationFeet Maximum range of distance that a child can be apart from the route.
 	 */
 	private static final long serialVersionUID = 1L;
 	double loiteringTime, deviationFeet;
@@ -15,49 +20,67 @@ public class ParamClass implements Serializable {
 		//
 	}
 	
+	/*
+	 * Constructor 1: Takes the maximum loitering time and deviation range. 
+	 */
 	public ParamClass( String loiteringTime, String deviationFeet)
 	{
-		//
 		this.loiteringTime = Double.parseDouble(loiteringTime);
 		this.deviationFeet = Double.parseDouble(deviationFeet);
 	}
 	
+	/*
+	 * Constructor 2: Takes the maximum loitering time and deviation range.
+	 */
 	public ParamClass( double loiteringTime, double deviationFeet)
 	{
-		//
 		this.loiteringTime = loiteringTime;
 		this.deviationFeet = deviationFeet;
 	}
 	
+	/*
+	 * setLoiteringTime method lets parent modify the new loitering time.
+	 */
 	public void setLoiteringTime(String loiteringTime)
 	{
-		//
 		this.loiteringTime = Double.parseDouble(loiteringTime);
 	}
 	
+	/*
+	 * setDeviationFeet method lets parent to modify the deviation range.
+	 */
 	public void setDeviationFeet(String deviationFeet)
 	{
-		//
 		this.deviationFeet = Double.parseDouble(deviationFeet);
 	}
 	
+	/*
+	 * setLoiteringTime method lets parent modify the new loitering time.
+	 */
 	public void setLoiteringTime(double loiteringTime)
 	{
-		//
 		this.loiteringTime = loiteringTime;
 	}
 	
+	/*
+	 * setDeviationFeet method lets parent modify the new deviation range.
+	 */
 	public void setDeviationFeet(double deviationFeet)
 	{
-		//
 		this.deviationFeet = deviationFeet;
 	}
 	
+	/*
+	 * getLoiteringTime method returns the time that the child has been loitering in a current spot.
+	 */
 	public double getLoiteringTime()
 	{
 		return loiteringTime; 
 	}
 	
+	/*
+	 * getDeviationFeet method returns how far the child is from nearest route point.
+	 */
 	public double getDeviationFeet()
 	{
 		return deviationFeet;
