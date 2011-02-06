a95c254b4bf6cf646b60dc3e00552056484585fa
heavyjeff17 <heavyjeff17@gmail.com>
2011-02-06 19:18:41 +0000

    Alerts setup on client side.

--- src/edu/MobileTracker.java
+++ src/edu/MobileTracker.java
@@ -180,6 +180,7 @@ public class MobileTracker extends Applet implements ActionListener {
 	public void alertNotify(String alertText)
 	{
 		//Show alert
+		JOptionPane.showConfirmDialog(this, alertText, "MobileTracker", 3, 0);
 	}
 
 	/**
