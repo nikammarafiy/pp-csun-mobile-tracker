5f83739a677f0204f51bb999edb8afb175716b5c
heavyjeff17@gmail.com <heavyjeff17@gmail.com@b3c8902e-b84e-8e89-50af-a8b036748bb7>
2011-02-06 01:05:30 +0000

    GEORoute commenting.

--- trunk/src/edu/GEORoute.java
+++ trunk/src/edu/GEORoute.java
@@ -4,32 +4,50 @@ import java.util.ArrayList;
 
 
 /**
- *
+ * Class that holds route data for the specified path
  * 
  */
 public class GEORoute {
 	
+	//Google url for map image
 	private final static String googURL = "http://maps.google.com/maps/api/staticmap?sensor=false&path=weight:3|color:green";
 
+	//Storage for the GEOPoints
     private ArrayList<GEOPoint> myRoute;
 
+    /**
+     * Default Constructor
+     */
     public GEORoute()
     {
-        //
         myRoute = new ArrayList<GEOPoint>();
     }
 
+    /**
+     * Get the size of the route
+     * @return The size of the route
+     */
     public int getLength()
     {
         return myRoute.size();
     }
 
+    /**
+     * Adds a GEOPoint to the end of the route
+     * @param tmpPoint The GEOPoint to add
+     * @return Success flag
+     */
     public boolean addPoint(GEOPoint tmpPoint)
     {
         myRoute.add(tmpPoint);
         return true;
     }
 
+    /**
+     * Returns a GEOPoint at the specified index
+     * @param i The index to get the GEOPoint from
+     * @return The requested GEOPoint
+     */
     public GEOPoint getPoint(int i)
     {
         if( i > myRoute.size() || i < 0)
@@ -40,6 +58,10 @@ public class GEORoute {
         return myRoute.get(i);
     }
     
+    /**
+     * Gets a static map url for this route
+     * @return A static map URL for this route
+     */
     public String getMapURL()
     {
     	//
